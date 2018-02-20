a6d48323fc6f521b5f1d316b3f66203cd349192c
Justin Hendrickson <justin.hendrickson@gmail.com>
2018-02-20 16:16:52 -0600

    Refactor builder to only write a method for fetching the list of confusable characters and replacements. Add unit tests.

---
+++ tests/FunctionsTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Confusables;
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionsTest extends TestCase
+{
+    /**
+     * @covers \Confusables\skeleton
+     * @uses \Confusables\get_confusables
+     * @uses \Confusables\unconfuse
+     */
+    public function testSkeleton()
+    {
+        $this->assertEquals(
+            'paypal',
+            skeleton('𝐩ɑ𝛾𝚙𝝰1')
+        );
+
+        $this->assertNotEquals(
+            'paypal',
+            skeleton('𝐩ɑУ𝚙𝝰1')
+        );
+    }
+
+    /**
+     * @covers \Confusables\unconfuse
+     * @uses \Confusables\get_confusables
+     */
+    public function testUnconfuse()
+    {
+        $this->assertEquals(
+            'paypal',
+            unconfuse('paypa1')
+        );
+
+        $this->assertNotEquals(
+            'paУpal',
+            unconfuse('paypal')
+        );
+    }
+
+    /**
+     * @covers \Confusables\is_confusable
+     * @uses \Confusables\get_confusables
+     * @uses \Confusables\skeleton
+     * @uses \Confusables\unconfuse
+     */
+    public function testIsConfusable()
+    {
+        $this->assertTrue(
+            is_confusable(
+                'ρ⍺у𝓅𝒂ן',
+                '𝔭𝒶ỿ𝕡𝕒ℓ'
+            )
+        );
+
+        $this->assertFalse(
+            is_confusable(
+                'jeff',
+                'mike'
+            )
+        );
+    }
+
+    /**
+     * @covers \Confusables\is_dangerous
+     * @uses \Confusables\get_confusables
+     * @uses \Confusables\skeleton
+     * @uses \Confusables\unconfuse
+     */
+    public function testIsDangerous()
+    {
+        $this->assertTrue(is_dangerous('𝔭𝒶ỿ𝕡𝕒ℓ'));
+        $this->assertFalse(is_dangerous('test'));
+    }
+}
